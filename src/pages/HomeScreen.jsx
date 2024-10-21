07b30cfca14038d0211dd3552d0bc7d72acf2ea1
mihaela-traia <king.mina.g@gmail.com>
2024-10-22 02:55:43 +0300

    add category section

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -4,6 +4,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import LandingSerum from "../assets/serum.png";
 import LandingMask from "../assets/mask.png";
+import Serums from "../assets/serum2.png";
+import EyeMasks from "../assets/eye mask2.png";
+import FaceCream from "../assets/face creams.png";
+import FaceMasks from "../assets/face masks.png";
 
 function Home() {
   return (
@@ -33,6 +37,53 @@ function Home() {
               <img src={LandingMask} alt="face mask" className="landing-img" />
             </Col>
           </Row>
+        </Container>
+      </section>
+      <section className="category">
+        <Container>
+          <h2 className="raleway-h2 text-center">Essence of Elegance</h2>
+          <p className="col-xl-6 col-lg-10 col-10 raleway-body text-center m-auto">
+            Indulge in the epitome of skincare luxury with LuxeGlow. Elevate
+            your routine with our meticulously crafted serums, rejuvenating eye
+            masks, sumptuous face creams, and enchanting masks. Experience
+            timeless beauty, refined.
+          </p>
+          <Row xs={2} md={2} lg={4} className="g-4 scroll">
+            <Col>
+              <Link to="/products" className="card opacity-75">
+                <img src={Serums} alt="face serum" />
+                <h3 className="raleway-h3 opacity-75">Serums</h3>
+                <div className="glow-effect"></div>
+              </Link>
+            </Col>
+            <Col>
+              <Link to="/products" className="card opacity-75">
+                <img src={EyeMasks} alt="eye mask" />
+                <h3 className="raleway-h3 opacity-75">Eye Masks</h3>
+                <div className="glow-effect"></div>
+              </Link>
+            </Col>
+            <Col>
+              <Link to="/products" className="card opacity-75">
+                <img src={FaceCream} alt="face cream" />
+                <h3 className="raleway-h3 opacity-75">Face Cream</h3>
+                <div className="glow-effect"></div>
+              </Link>
+            </Col>
+            <Col>
+              <Link to="/products" className="card opacity-75">
+                <img src={FaceMasks} alt="face mask" />
+                <h3 className="raleway-h3 opacity-75">Face Masks</h3>
+                <div className="glow-effect"></div>
+              </Link>
+            </Col>
+          </Row>
+          <div className="button-container d-flex justify-content-center">
+            <Link to="/products" className="button raleway-button">
+              See all
+              <span className="material-symbols-outlined">arrow_forward</span>
+            </Link>
+          </div>
         </Container>
       </section>
     </Layout>
